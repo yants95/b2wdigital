55c605adacfbaf04c470a3a4866d69b5f18afbb1
Yan Soares <yts.1995@gmail.com>
2021-01-13 22:07:43 -0300

    test: ensure DbSearchPlanetById calls SearchByIdRepository with correct id

---
+++ src/data/usecases/planet/search-planet-by-id/search-planet-by-id.spec.ts
@@ -0,0 +1,27 @@
+import { DbSearchPlanetByIdRepository } from "@/data/db/planet/search-planet-by-id-repository"
+import { SearchPlanetByNameRepository } from "@/data/db/planet/search-planet-by-name-repository"
+import { mockSearchPlanetByIdRepository } from "@/data/test/mock-planet"
+import { DbSearchPlanetById } from "./search-planet-by-id"
+
+type SutTypes = {
+  sut: DbSearchPlanetById
+  searchPlanetByIdRepository: DbSearchPlanetByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const searchPlanetByIdRepository = mockSearchPlanetByIdRepository()
+  const sut = new DbSearchPlanetById(searchPlanetByIdRepository)
+  return {
+    sut,
+    searchPlanetByIdRepository,
+  }
+}
+
+describe('DbSearchPlanetById Usecase', () => {
+  test('shoud search a planet by its name', async () => {
+    const { sut, searchPlanetByIdRepository } = makeSut()
+    const searchSpy = jest.spyOn(searchPlanetByIdRepository, 'searchById')
+    await sut.searchById('any_id')
+    expect(searchSpy).toHaveBeenCalledWith('any_id')
+  })
+})
