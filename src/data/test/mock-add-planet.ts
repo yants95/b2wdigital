bddd4447a8fef94051e0d305c5a0e920a88e6b03
Yan Soares <yts.1995@gmail.com>
2021-01-14 22:51:19 -0300

    chore: add more fields to planet entity

--- src/data/test/mock-add-planet.ts
+++ src/data/test/mock-add-planet.ts
@@ -5,7 +5,17 @@ import { PlanetModel } from "../models/planet"
 export const mockAddPlanetParams = (): AddPlanetParams => ({
     "name": "any_name",
     "climate": "any_climate",
-    "terrain": "any_terrain"
+    "terrain": "any_terrain",
+    "rotation_period": "string",
+    "orbital_period": "string",
+    "diameter": "string",
+    "gravity": "string",
+    "surface_water": "string",
+    "population": "string",
+    "films": ["any_film", "other_film"],
+    "created": new Date('2021-01-13'),
+    "edited": new Date('2021-01-13'),
+    "url": "any_url"
 })
 
 export const mockAddPlanetRepository = (): AddPlanetRepository => {
--- src/domain/entities/planet.ts
+++ src/domain/entities/planet.ts
@@ -1,4 +1,14 @@
 export type Planet = {
+    rotation_period: string
+    orbital_period: string
+    diameter: string
+    gravity: string
+    surface_water: string
+    population: string
+    films: string[]
+    created: Date
+    edited: Date
+    url: string
     name: string
     climate: string
     terrain: string
