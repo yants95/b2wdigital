eb27460e2f34f1f55b7a61bb496c7eb5026b461a
Yan Soares <yts.1995@gmail.com>
2021-01-16 12:00:12 -0300

    feat: add PlanetMongoRepository

--- src/infra/db/mongodb/planet/planet-mongo-repository.ts
+++ src/infra/db/mongodb/planet/planet-mongo-repository.ts
@@ -1,26 +1,47 @@
 import { LoadPlanetsRepository } from "@/data/db/planet/load-planets-repository"
 import { Planet } from "@/domain/entities/planet"
+import { AddPlanet, AddPlanetParams } from "@/domain/usecases/planet/add-planet"
+import { ListPlanets } from "@/domain/usecases/planet/list-planets"
 import { SearchPlanetById } from "@/domain/usecases/planet/search-planet-by-id"
 import { SearchPlanetByName } from "@/domain/usecases/planet/search-planet-by-name"
 import env from '@/main/config/env'
 import axios from 'axios'
+import { ObjectId } from "mongodb"
 import { MongoHelper } from "../helpers/mongo-helper"
 
-export class PlanetMongoRepository implements LoadPlanetsRepository, SearchPlanetByName, SearchPlanetById {
-    async load (): Promise<Planet[]> {
+export class PlanetMongoRepository implements LoadPlanetsRepository, SearchPlanetByName, SearchPlanetById, AddPlanet, ListPlanets {
+    async load (): Promise<any> {
         const response = await axios.get(`${env.swapiAPI}/planets`)
-        return response.data
+        const planetCollection = await MongoHelper.getCollection('planets')
+
+        if (!planetCollection.find()) {
+            await planetCollection.insertMany(response.data.results)
+        }
+        
+        return 'carregou'
     }
 
-    async searchByName (planetName: string) : Promise<Planet> {
+    async searchByName (planetName: string): Promise<Planet> {
         const planetCollection = await MongoHelper.getCollection('planets')
         const planet = await planetCollection.findOne({ name: planetName })
         return planet && MongoHelper.map(planet)
     }
 
-    async searchById (planetId: string) : Promise<Planet> {
+    async searchById (planetId: string): Promise<Planet> {
         const planetCollection = await MongoHelper.getCollection('planets')
-        const planet = await planetCollection.findOne({ _id: planetId })
+        const planet = await planetCollection.findOne({ _id: new ObjectId(planetId) })
         return planet && MongoHelper.map(planet)
+    }
+
+    async add (planetParams: AddPlanetParams): Promise<Planet> {
+        const planetCollection = await MongoHelper.getCollection('planets')
+        const planet = await planetCollection.insertOne(planetParams)
+        return planet && MongoHelper.map(planet)
+    }
+
+    async list (): Promise<Planet[]> {
+        const planetCollection = await MongoHelper.getCollection('planets')
+        const planets = await planetCollection.find({})
+        return planets && MongoHelper.map(planets)
     }
 }
