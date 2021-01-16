4f59c5f1be95adfd2556e770111d5b752a4f5da2
Yan Soares <yts.1995@gmail.com>
2021-01-16 11:55:06 -0300

    feat: add http-helper methods

---
+++ src/presentation/helpers/http/http-helper.ts
@@ -0,0 +1,11 @@
+import { HttpResponse } from "@/presentation/protocols/http";
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
+})
